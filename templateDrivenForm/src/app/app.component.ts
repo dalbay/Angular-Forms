4668654a1aef8d66326af8456809d8d6cbcbd985
dalbay <fadalbay@gmail.com>
2020-04-17 22:52:45 -0400

    user module

--- templateDrivenForm/src/app/app.component.ts
+++ templateDrivenForm/src/app/app.component.ts
@@ -1,16 +1,9 @@
-import { Component, NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
-})
-@NgModule({
-  declarations: [AppComponent],
-  imports: [FormsModule],
-  providers: [],
-  bootstrap: [AppComponent],
 })
 export class AppComponent {
   topics = ["Angular", "React", "Vue"];
